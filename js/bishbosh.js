09fef13271641367a2e418e779c1536fb542ed9a
Pekka Simola <pekka1simola@gmail.com>
2022-01-17 21:31:03 +0100

    finished version

--- js/bishbosh.js
+++ js/bishbosh.js
@@ -1,10 +1,39 @@
 'use strict';
 
-let bishBosh = function(bish, bosh, loopCount){
+// Link settings in lots of 3: linkname, textbox, std-value
+const linkClicks = ['bishLink', 'bish', '3', 'bishLink2', 'bish', '3'
+                  , 'boshLink', 'bosh', '4', 'boshLink2', 'bosh', '4'
+                  , 'endValueLink', 'endValue', '100'];
+
+// Link events; auto-activate the correct text-box; missing values are replaced by std-values
+(function () {
+    let o;
+    for (let i = 0; i < linkClicks.length; i += 3){
+        document.getElementById(linkClicks[i]).addEventListener('click',
+            function ()
+            {
+                o = document.getElementById(linkClicks[i + 1]);
+                o.focus();  // sets marker into the box
+                o.select(); // selects available content
+                if (!o.checkValidity() || o.value.length == 0)
+                    o.value = linkClicks[i + 2]; // standard value to empty or erroric text-box
+            }
+        , false)
+    }
+})()
+
+function calcBishBash(){
+
+    // these values are validated
+    const bish = document.getElementById("bish").value;
+    const bosh = document.getElementById("bosh").value;
+    const endValue = document.getElementById("endValue").value;
+
     let iBish;
     let iBosh;
     let bB = [];
-    for (let i = 1; i <= loopCount; i++){
+
+    for (let i = 1; i <= endValue; i++){
         iBish = i % bish;
         iBosh = i % bosh;
         if((iBish + iBosh) == 0){
@@ -17,20 +46,27 @@ let bishBosh = function(bish, bosh, loopCount){
             bB.push(i.toString());
         }
     }
-    return bB.join(', ');
+    let p = document.getElementById("result")
+    p.innerText = bB.join(', ');
+    p.focus(); // anything but the btn
+    
 }
 
-console.log(bishBosh(3, 4, 100));
+// Validation of form-textboxes triggered by btnBishBosh-click
+(function () {
+    var forms = document.querySelectorAll('.needs-validation') 
 
-/*
-    Webb-formulär för input av bish, bosh och loopCount;
-    Inga negativa tal tillåts
-    loopCount intervall: 100 till ca 1000; 
-    bish bör vara mindre än bosh och båda definitivt mindre än loopCount!
-    ● Ett webbaserat UI som innehåller:
-        ○ Inputfält för ovanstående variabler
-        ○ En header (Bish-Bosh 2.0)
-        ○ En footer med ert namn och copyright
-        ○ En text som förklarar ‘Bish-Bosh’ och i vilket syfte ni skapat sidan.
-        ○ En presentation av talen
-*/
+    Array.prototype.slice.call(forms).forEach(function (form)
+    {
+        form.addEventListener('submit', function (event)
+        {
+            if (!form.checkValidity())
+            {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated')
+        }
+        , false)
+    })
+})()
